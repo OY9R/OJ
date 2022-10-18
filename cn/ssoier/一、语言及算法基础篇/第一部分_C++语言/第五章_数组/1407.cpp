6fe4b4db9a911d30b502e7853a5e2d7dcd079aff
OY9R <9rouyang@gmail.com>
2022-10-18 23:50:05 +0800

    update

--- cn/ssoier/一、语言及算法基础篇/第一部分_C++语言/第五章_数组/1407.cpp
+++ cn/ssoier/一、语言及算法基础篇/第一部分_C++语言/第五章_数组/1407.cpp
@@ -1,33 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define N 105 
-bool isPrime(int n)
-{
-	if(n < 2)
-		return false;
+bool isPrime(int n){
+	if(n < 2) return false;
 	for(int i = 2; i <= sqrt(n); ++i)
-		if(n%i == 0)
-			return false;
+		if(n%i == 0) return false;
 	return true;
 }
-int main()
-{
-	char s[N];
+int main(){
+	string s;
 	cin >> s;
-	int chNum[128] = {}, len = strlen(s), maxn = 0, minn = N;
+	int chNum[128] = {}, len = s.size(), maxn = 0, minn = N;
 	for(int i = 0; i < len; ++i)
 		chNum[s[i]]++; 
 	for(char i = 'a'; i <= 'z'; ++i)
-	{
-		if(chNum[i] > 0)
-		{
+		if(chNum[i] > 0){
 			maxn = max(maxn, chNum[i]);
 			minn = min(minn, chNum[i]);
 		}
-	}
-	if(isPrime(maxn - minn))
-		cout << "Lucky Word" << endl << maxn - minn;
-	else
-		cout << "No Answer" << endl << 0;
+	if(isPrime(maxn - minn)) cout << "Lucky Word" << endl << maxn - minn;
+	else cout << "No Answer" << endl << 0;
 	return 0;
 }
